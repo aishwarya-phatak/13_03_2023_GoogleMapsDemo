48f88ec8a2430aedeca9f1d5d0309aecc3b83c0f
aishwarya <ashp17star@gmail.com>
2023-05-24 12:38:48 +0530

    Drawing Polygon, PolyLine, Circle and Zoom in Feature by using GMSCameraPosition

--- 13_03_2023_GoogleMapsDemo/ViewController.swift
+++ 13_03_2023_GoogleMapsDemo/ViewController.swift
@@ -14,11 +14,17 @@ class ViewController: UIViewController {
     var marker : GMSMarker = GMSMarker()
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self
+        //mapView.delegate = self
         initializeMapSettings()
-        let cameraPostion = GMSCameraPosition(latitude: 18.5091, longitude: 73.8326, zoom: 20.0)
+        let cameraPostion = GMSCameraPosition(latitude: 27.1751, longitude: 78.0421, zoom: 20.0)
+        mapView.camera = cameraPostion
         GMSCameraUpdate.setTarget(cameraPostion.target)
         showMarker(position: cameraPostion.target)
+
+        
+        drawPolygonOnMap()
+        drawPolylineOnMap()
+        drawCircleOnMap()
       }
     
     func showMarker(position : CLLocationCoordinate2D){
@@ -60,19 +66,21 @@ extension ViewController : GMSMapViewDelegate{
         infoWindow.layer.borderColor = CGColor(red: 100.0, green: 0.0, blue: 100.0, alpha: 6.0)
         infoWindow.layer.borderWidth = 2.0
         
-        let infoLabel = UIView(frame: CGRect(x: 10, y: 10, width: Int(infoWindow.frame.width - 50), height: 40))
+        let infoLabel = UILabel(frame: CGRect(x: 10, y: 10, width: Int(infoWindow.frame.width - 50), height: 40))
         infoLabel.backgroundColor = UIColor.init(red: 0.0, green: 10.0, blue: 90.0, alpha: 6.0)
         infoLabel.layer.borderColor = CGColor(red: 0.0, green: 0.0, blue: 200.0, alpha: 20.0)
         infoLabel.layer.borderWidth = 2.0
+        infoLabel.text = "Bitcode"
         infoWindow.addSubview(infoLabel)
         
 //        let secondInfoLabel = UIView(frame: CGRect(x: 10, y: 60, width: infoLabel.frame.width, height: 35))
-        let secondInfoLabel = UIView(frame: CGRect(x: infoLabel.frame.minX, y:infoLabel.frame.height + 10, width: infoLabel.frame.width, height: 35))
+        let secondInfoLabel = UILabel(frame: CGRect(x: infoLabel.frame.minX, y:infoLabel.frame.height + 10, width: infoLabel.frame.width, height: 35))
         
         secondInfoLabel.backgroundColor = .brown
         secondInfoLabel.alpha = 2.0
         secondInfoLabel.layer.borderColor = CGColor(red: 0.0, green: 0.0, blue: 100.0, alpha: 4.0)
         secondInfoLabel.layer.borderWidth = 2.0
+        secondInfoLabel.text = "Software Training"
         infoWindow.addSubview(secondInfoLabel)
         
         return infoWindow
@@ -84,5 +92,44 @@ extension ViewController : GMSMapViewDelegate{
     
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
         print("Lat :\(coordinate.latitude) - Long :\(coordinate.longitude)")
+    }
+    
+    func drawPolygonOnMap(){
+       let polygonPath = GMSMutablePath()
+        polygonPath.add(CLLocationCoordinate2D(latitude: 19.7515, longitude: 75.7139))
+        polygonPath.add(CLLocationCoordinate2D(latitude: 22.6708, longitude: 71.5724))
+        polygonPath.add(CLLocationCoordinate2D(latitude: 27.0238, longitude: 74.2179))
+        polygonPath.add(CLLocationCoordinate2D(latitude: 22.9734, longitude: 78.6569))
+        
+       let polygon = GMSPolygon(path: polygonPath)
+        polygon.fillColor = .brown
+        polygon.layer.opacity = 3.0
+        polygon.strokeColor = .white
+        polygon.strokeWidth = 3.0
+        polygon.map = mapView
+    }
+    
+    func drawPolylineOnMap(){
+       let polyLinePath = GMSMutablePath()
+        polyLinePath.add(CLLocationCoordinate2D(latitude: 18.5204, longitude: 73.8567))
+        polyLinePath.addLatitude(19.10107, longitude: 74.74067)
+        polyLinePath.add(CLLocationCoordinate2D(latitude: 17.659920, longitude: 75.90638))
+        polyLinePath.add(CLLocationCoordinate2D(latitude: 17.691401, longitude: 74.000938))
+        polyLinePath.add(CLLocationCoordinate2D(latitude: 18.5204, longitude: 73.8567))
+       let polyLine = GMSPolyline(path: polyLinePath)
+        polyLine.strokeWidth = 3.0
+        polyLine.strokeColor = .red
+        polyLine.zIndex = 90
+        polyLine.map = mapView
+    }
+
+    func drawCircleOnMap(){
+       let circle = GMSCircle(position: CLLocationCoordinate2D(latitude: 27.1751, longitude: 78.0421), radius: 20.0)
+        circle.strokeColor = .black
+        circle.strokeWidth = 5.0
+        circle.fillColor = .brown
+        circle.zIndex = 40
+        circle.map = mapView
+        circle.map?.layer.opacity = 2.0
     }
 }
